ecdf06f111b6e3114d9b6a9b009fc9bcd97e95f9
RawanMostafa08 <rawanmostafa401@gmail.com>
2024-09-28 20:33:43 +0300

    doc: Adding end user documentation

--- cmd/main.go
+++ cmd/main.go
@@ -8,12 +8,15 @@ import (
 
 func main() {
 	cpuInfo, _ := psutils.GetCpuInfo()
-	log.Printf("%v",cpuInfo)
+	log.Printf("%v", cpuInfo)
 	memInfo, err := psutils.GetMemInfo()
-	log.Printf("%v",memInfo)
-	log.Printf("err %v",err)
+	log.Printf("%v", memInfo)
+	log.Printf("err %v", err)
 	procs, err := psutils.GetProcessList()
-	log.Printf("%v\n",procs)
-	log.Printf("err %v",err)
+	log.Printf("%v\n", procs)
+	log.Printf("err %v", err)
+	det, err := psutils.GetProcessDetails(1)
+	log.Printf("%v\n", det)
+	log.Printf("err %v", err)
 
 }
